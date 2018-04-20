30420d3e4d83fc0db09560be82b141f57608ab41
OwO <OwO@OmO.tw>
2018-04-21 01:17:11 +0800

    [checkHelper] fix checking list

--- CheckHelper.cs
+++ CheckHelper.cs
@@ -12,10 +12,16 @@ namespace CNBlackListSoamChecker
                 foreach (long i in Temp.adminInReport)
                     if (i == ChatID)
                         return true;
+
+                foreach (long i in Temp.adminChecking)
+                    if (i == ChatID)
+                        return true;
+                
+                Temp.adminChecking.Add(ChatID);
                 
                 bool status = false;
                 GroupUserInfo[] admins = TgApi.getDefaultApiConnection().getChatAdministrators(ChatID,true);
-                System.Console.WriteLine("Getting Chat Administrator List ChatID : " + ChatID);
+                System.Console.WriteLine("[checkHelper] Getting Chat Administrator List ChatID : " + ChatID);
                 foreach (var admin in admins)
                 {
                     if (admin.user.id != TgApi.getDefaultApiConnection().getMe().id)
@@ -55,7 +61,8 @@ namespace CNBlackListSoamChecker
                 else
                     System.Console.WriteLine("[checkHelper] Admin not in report group GID : " + ChatID.ToString());
 
-
+                Temp.adminChecking.Remove(ChatID);
+                
                 return status;
 
             }
