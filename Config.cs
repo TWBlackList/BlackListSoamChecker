66498adcae429589f2659df983d38791b65be549
OwO <OwO@OmO.tw>
2018-05-16 21:47:54 +0800

    [config] typo

--- Config.cs
+++ Config.cs
@@ -3,7 +3,7 @@ using BlackListSoamChecker.DbManager;
 using System.Runtime.Serialization.Json;
 using System.IO;
 using System.Text;
-using System.Linq.Eumerable;
+using System.Linq.Enumerable;
 
 namespace BlackListSoamChecker
 {
