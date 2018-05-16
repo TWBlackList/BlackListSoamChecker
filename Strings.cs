e23585a30cd3a5422822346c7b271a41d615d4d2
OwO <OwO@OmO.tw>
2018-05-17 01:40:58 +0800

    fix

--- Strings.cs
+++ Strings.cs
@@ -11,7 +11,6 @@ namespace BlackListSoamChecker
             {
                 if (Config.Language == "TW")
                 {
-                    lang = new LanguageData();
                     tmp = lang;
                 }
             }
