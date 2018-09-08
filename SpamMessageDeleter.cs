ce7d10ebb8c4a83bad31abf929d23f2863b43f43
OwO <OwO@OmO.tw>
2018-09-09 02:57:45 +0800

    [spamDeleter] fix typo

--- SpamMessageDeleter.cs
+++ SpamMessageDeleter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using BlackListSoamChecker.DbManager;
+using BlackListSoamChecker.CommandObject;
 using ReimuAPI.ReimuBase;
 using ReimuAPI.ReimuBase.Interfaces;
 using ReimuAPI.ReimuBase.TgData;
